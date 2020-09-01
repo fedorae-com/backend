dcb45b88c506991736ef01cc872d9b455fdc9477
froshims <lazarusgrey37@gmail.com>
2020-09-01 06:53:00 -0700

    Added product model

--- db/main.go
+++ db/main.go
@@ -1,34 +1,29 @@
 package db
 
 import (
-  "database/sql"
-  "fmt"
-
-  _ "github.com/lib/pq"
+	"github.com/go-pg/pg"
+	"log"
+	"os"
 )
 
-const (
-  host     = "localhost"
-  port     = 5432
-  user     = "postgres"
-  password = "P0$tGr3s"
-  dbname   = "fedorae"
-)
+func Connect()  {
+	opts := &pg.Options {
+		User: "postgres",
+		Password: "P0$tGr3s",
+		Addr: "localhost:5432",
+	}
 
-func Connect() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
-
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-
-  fmt.Println("Successfully connected!")
+	var db *pg.DB = pg.Connect(opts)
+	if db == nil {
+		log.Printf("Database connection failed.\n")
+		os.Exit(100)
+	}
+	log.Printf("Connect successful.\n")
+	closeErr := db.Close()
+	if closeErr != nil {
+		log.Printf("Error while closing connection, %v\n", closeErr)
+		os.Exit(100)
+	}
+	log.Printf("Connection closed successfully.\n")
+	return
 }
